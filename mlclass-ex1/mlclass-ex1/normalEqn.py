b2b747fc0b2bd32670983085723d151c78d85d02
arturomp <amp.interwebs@gmail.com>
2015-06-04 16:55:26 -0400

    initial commit
    exercises’ files + python implementation of exercise 1 (batch gradient
    descent) from original in octave

---
+++ mlclass-ex1/mlclass-ex1/normalEqn.py
@@ -0,0 +1,23 @@
+def normalEqn(X, y):
+#NORMALEQN Computes the closed-form solution to linear regression 
+#   NORMALEQN(X,y) computes the closed-form solution to linear 
+#   regression using the normal equations.
+    
+    import numpy as np 
+    theta = np.zeros((X.shape[1], 1)); 
+
+# ====================== YOUR CODE HERE ======================
+# Instructions: Complete the code to compute the closed form solution
+#               to linear regression and put the result in theta.
+#
+
+# ---------------------- Sample Solution ----------------------
+
+    theta = np.linalg.pinv(np.transpose(X).dot(X)).dot(np.transpose(X).dot(y))
+
+    return theta
+
+# -------------------------------------------------------------
+
+
+# ============================================================
