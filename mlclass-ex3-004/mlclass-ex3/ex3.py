eef5fb543c9b02c39d7e1e06b8c64d56b4c06d6c
arturomp <amp.interwebs@gmail.com>
2015-09-15 20:37:17 -0500

    commented out section visualizing y and predictions

--- mlclass-ex3-004/mlclass-ex3/ex3.py
+++ mlclass-ex3-004/mlclass-ex3/ex3.py
@@ -56,7 +56,7 @@ raw_input('Program paused. Press enter to continue.\n')
 
 print('Training One-vs-All Logistic Regression...')
 
-lambda_reg = 0.1
+lambda_reg = 1
 all_theta = ova.oneVsAll(X, y, num_labels, lambda_reg)
 
 raw_input('Program paused. Press enter to continue.\n')
@@ -65,4 +65,27 @@ raw_input('Program paused. Press enter to continue.\n')
 #  After ...
 pred = pova.predictOneVsAll(all_theta, X)
 
-print('Training Set Accuracy: {:f}'.format(np.mean(pred == y.flatten()%10) * 100))
+print('Training Set Accuracy: {:f}'.format((pred == y.flatten()%10).sum()/100))
+print('Training Set Accuracy for 1: {:f}'.format(np.mean(pred[500:1000] == y.flatten()[500:1000]%10) * 100))
+print('Training Set Accuracy for 2: {:f}'.format(np.mean(pred[1000:1500] == y.flatten()[1000:1500]%10) * 100))
+print('Training Set Accuracy for 3: {:f}'.format(np.mean(pred[1500:2000] == y.flatten()[1500:2000]%10) * 100))
+print('Training Set Accuracy for 4: {:f}'.format(np.mean(pred[2000:2500] == y.flatten()[2000:2500]%10) * 100))
+print('Training Set Accuracy for 5: {:f}'.format(np.mean(pred[2500:3000] == y.flatten()[2500:3000]%10) * 100))
+print('Training Set Accuracy for 6: {:f}'.format(np.mean(pred[3000:3500] == y.flatten()[3000:3500]%10) * 100))
+print('Training Set Accuracy for 7: {:f}'.format(np.mean(pred[3500:4000] == y.flatten()[3500:4000]%10) * 100))
+print('Training Set Accuracy for 8: {:f}'.format(np.mean(pred[4000:4500] == y.flatten()[4000:4500]%10) * 100))
+print('Training Set Accuracy for 9: {:f}'.format(np.mean(pred[4500:5000] == y.flatten()[4500:5000]%10) * 100))
+print('Training Set Accuracy for 10: {:f}'.format(np.mean(pred[0:500] == y.flatten()[0:500]%10) * 100))
+
+# np.set_printoptions(threshold=np.nan)
+
+# tmp_1 = np.column_stack((y, pred))
+# tmp_2 = np.column_stack((tmp_1,pred == y.flatten()%10)) 
+# tmp_3 = np.zeros((tmp_2.shape[0]))
+# for i in range(tmp_2.shape[0]):
+#     if i == 0:
+#         tmp_3[i] = tmp_2[i,2]
+#     else:
+#         tmp_3[i] = tmp_3[i-1] + tmp_2[i,2]
+# tmp_4 = np.column_stack((tmp_2,tmp_3))
+# print (tmp_4)
