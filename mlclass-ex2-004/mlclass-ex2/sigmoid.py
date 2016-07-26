3cf109d226c56fc23be01c46c9cc799e62e0c19a
arturomp <amp.interwebs@gmail.com>
2016-07-26 16:46:43 -0400

    ex2 multi-class classification and neural net prediction

--- mlclass-ex2-004/mlclass-ex2/sigmoid.py
+++ mlclass-ex2-004/mlclass-ex2/sigmoid.py
@@ -12,6 +12,7 @@ def sigmoid(z):
 # Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 #               vector or scalar).
 
+
     # g = 1/(1 + np.exp(-z))
     g = expit(z)
 
