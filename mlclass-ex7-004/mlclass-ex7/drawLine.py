7d1cc60805aa3d1808442403507ef064a44669d1
arturomp <amp.interwebs@gmail.com>
2016-08-20 18:53:33 -0400

    ex7 k-means clustering + principal component analysis (pca)
    proper management of keyword arguments

--- mlclass-ex7-004/mlclass-ex7/drawLine.py
+++ mlclass-ex7-004/mlclass-ex7/drawLine.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 
-def drawLine(p1, p2, varargin=None):
+def drawLine(p1, p2, **kwargs):
 #DRAWLINE Draws a line from point p1 to point p2
 #   DRAWLINE(p1, p2) Draws a line from point p1 to point p2 and holds the
 #   current figure
 
-    plt.plot([p1[0], p2[0]], [p1[1], p2[1]], c='b')
+    plt.plot([p1[0], p2[0]], [p1[1], p2[1]], **kwargs)
